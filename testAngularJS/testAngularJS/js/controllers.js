6a9b757bf9c0111c8f3b693036c5f0b2085ddf2a
Sebastian Sandberg <sebastian.sandberg@sigma.se>
2014-09-30 10:53:41 +0200

    Addad project testAngularJS
    Change-Id: Ic3aea56d80c90a7a9a02abfd44545d8bca106767

---
+++ testAngularJS/testAngularJS/js/controllers.js
@@ -0,0 +1,21 @@
+﻿var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('PhoneListCtrl', function ($scope, $http) {
+  $http.get('phones/phones.json').success(function (data) {
+    $scope.phones = data;
+  });
+
+  $scope.orderProp = 'age';
+
+});
+
+//var phonecatApp = angular.module('phonecatApp', []);
+
+//phonecatApp.controller('phoneListCtrl', ['$scope', '$http',
+//  function ($scope, $http) {
+//    $http.get('phones/phones.json').success(function(data) {
+//      $scope.phones = data;
+//    });
+
+//    $scope.orderProp = 'age';
+//  }]);
---
+++ testAngularJS/testAngularJS/test/unit/controllerSpec.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+
+describe('PhoneListCtrl', function () {
+
+  beforeEach(module('phonecatApp'));
+
+  it('should create "phones" model with 3 phones', inject(function ($controller) {
+    var scope = {},
+        ctrl = $controller('PhoneListCtrl', { $scope: scope });
+
+    expect(scope.phones.length).toBe(3);
+    expect(scope.name).toBe('World')
+  }));
+
+});
