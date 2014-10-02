5bc7a42fa04f0b9c32201a5efd3e945cda8de055
Sebastian Sandberg <sebastian.sandberg@sigma.se>
2014-10-02 11:26:46 +0200

    Added alot more
    Change-Id: Ic94aa9f6f979b4da4c2b8b35076558b9fb453803

---
+++ testAngularJS/testAngularJS/js/services.js
@@ -0,0 +1,10 @@
+﻿'use strict';
+
+var phonecatServices = angular.module('phonecatServices', ['ngResource']);
+
+phonecatServices.factory('Phone', ['$resource',
+  function ($resource) {
+    return $resource('phones/:phoneId.json', {}, {
+      query: { method: 'GET', params: { phoneId: 'phones' }, isArray: true }
+    });
+  }]);
